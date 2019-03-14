3817746d0d5c7d0e8621508a20302ea11967ee28
Krishna Teja <teja.np18@gmail.com>
2019-03-14 02:00:31 -0400

    Registration and login pages

---
+++ server/src/config.js
@@ -0,0 +1,19 @@
+const mysql      = require('mysql');
+         require('dotenv').load();
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
+  connectionLimit: 10,
+  insecureAuth: true
+});
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected");
+} else {
+    console.log("Error while connecting with database");
+}
+});
+module.exports = connection;
